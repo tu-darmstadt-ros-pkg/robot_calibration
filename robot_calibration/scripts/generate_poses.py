d3fa79aea44f22d83cc4abec9932bfa4d2eabfd7
Martin Oehler <martin.sven.oehler@gmail.com>
2016-11-21 16:15:14 +0100

    pose generator script works now

--- robot_calibration/scripts/generate_poses.py
+++ robot_calibration/scripts/generate_poses.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 import rosbag
 import actionlib
@@ -12,12 +14,13 @@ action_topic = '/combined_planner'
 frame_ids = ['chilitag{}_link'.format(i) for i in range(2)]
 poses_per_frame = 10
 
+client_timeout = 5
+request_timeout = 30
+
 
 class CapturePoses:
     def __init__(self):
         self.last_state_ = CaptureConfig()
-
-        rospy.init_node('generate_calibration_poses')
         rospy.Subscriber("joint_states", JointState, self.state_cb)
 
         self.count = 0
@@ -32,8 +35,8 @@ class CapturePoses:
             print('Joint state is empty. Can\'t save it.')
         else:
             self.bag.write('calibration_joint_states', self.last_state_)
-            print("Saving pose %d" % self.count)
             self.count += 1
+            print("-- Saving pose %d" % self.count)
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.bag.close()
@@ -50,33 +53,38 @@ class CapturePoses:
 
 
 def create_action_goal():
-    goal = argo_move_group_msgs.msg.ArgoCombinedPlanActionGoal()
-    goal.goal.object_type = 4
-    goal.goal.action_type = 2
-    goal.goal.target.header.frame_id = frame
-    goal.goal.target.pose.position.z = 0.001
+    goal = argo_move_group_msgs.msg.ArgoCombinedPlanGoal()
+    goal.object_type.val = 4
+    goal.action_type.val = 2
+    goal.target.header.frame_id = frame
+    goal.target.pose.position.z = 0.001
     return goal
 
 
 if __name__ == '__main__':
+    rospy.init_node('generate_calibration_poses')
     # create action client
     client = actionlib.SimpleActionClient(action_topic, argo_move_group_msgs.msg.ArgoCombinedPlanAction)
-    print("Waiting 5 seconds for action server on topic", action_topic)
-    client.wait_for_server(rospy.Duration.from_sec(5))
+    print("Waiting", client_timeout, "seconds for action server on topic", action_topic)
+    client.wait_for_server(rospy.Duration.from_sec(client_timeout)) #TODO warning if no connection
 
     # create bag manager
     with CapturePoses() as bag:
         for frame in frame_ids:
+            print("## Generating", poses_per_frame, "poses for frame", frame)
             for i in range(poses_per_frame):
                 # send action
                 client.send_goal(create_action_goal())
-                print("Send goal. Waiting 5s for the result.")
-                client.wait_for_result(rospy.Duration.from_sec(5))
-                result = client.get_result()
-
-                success = result.val == argo_move_group_msgs.msg.ErrorCodes.SUCCESS
-                if success:
-                    bag.capture_pose()
+                print("Planning pose", i, ". Waiting", request_timeout, "seconds for the result.")
+                client.wait_for_result(rospy.Duration.from_sec(request_timeout))
+                result = client.get_result() #TODO catch none
+                if result is None:
+                    print("-- No result received in time.")
                 else:
-                    print("Observation planning failed with error code", result.val)
-                    continue
+                    success = (result.success.val == argo_move_group_msgs.msg.ErrorCodes.SUCCESS)
+                    if success:
+                        bag.capture_pose()
+                    else:
+                        print("-- Observation planning failed with error code", result.success.val)
+                        # TODO repeat planning x times?
+                        continue
